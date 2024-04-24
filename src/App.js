7141a174b5fa051e278ecf2bf6a87ffb6c8ad0a3
Faizan Chaudary <83314898+faizan-debug@users.noreply.github.com>
2024-04-24 13:04:06 +0500

    User data show page
    It will show data of any user when you click on profile
    User Repo is created and data showed from repo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import User from './components/users/User';
 import axios from 'axios';
 import './App.css';
 
@@ -13,31 +14,52 @@ class App extends Component {
 
   state = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
     alert: null
   }
 
+  //Function to show random users o page
   async componentDidMount () {
     this.setState ({ loading: true});
     const res = await axios.get (`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     this.setState ({ users: res.data, loading: false});
   }
 
+  //Function is called from search component by passing props up
   searchUsers = async (text) => {
     this.setState ({ loading: true});
     const res = await axios.get (`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-    this.setState ({ users: res.data.items, loading: false});
+    this.setState ({ users: res.data.items, loading: false})
+    console.log(res.data);
   }
 
+  //Function is called from user Item component to display user details
+  getUser = async (username) => {
+    this.setState ({ loading: true});
+    const res = await axios.get (`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState ({ user: res.data, loading: false});
+  }
+
+  //Function is called from user.js component to display the latest repos from Github
+  getUserRepos = async (username) => {
+    this.setState ({ loading: true});
+    const res = await axios.get (`https://api.github.com/users/${username}/repos?per_page=5&sort=created%20asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState ({ repos: res.data, loading: false});
+  }
+
+  //Function is called from search component to clear users from state
   clearUsers = () => this.setState({users: [], loading: false})
 
+  //Function is called from search component to raise an alert fro empty text field
   setAlert = (msg, type) => {
     this.setState({alert: {msg, type}})
     setTimeout (() => this.setState({alert: null}), 5000)
   }
   
   render () {
-    const {users, loading} = this.state;
+    const {users, loading, user, repos} = this.state;
     return (
       <Router>
       <div className="App">
@@ -60,6 +82,12 @@ class App extends Component {
             </Route>
              {/*Route for the Home page */}
              <Route path='/about' Component={About}/> 
+
+             {/*Route for user detail page */}
+             <Route exact path='/user/:login' element={ props => (
+              <User {...props} getUser={this.getUser} getUserRepos={this.getUserRepos} repos={repos} user={user} loading={loading}/>
+             )}/> 
+
 
           </Routes>
        
---
+++ src/components/repos/RepoItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const RepoItem = ( {repo} ) => {
+  return (
+    <div className='card'>
+        <h1>
+            <a href={repo.html_url} target='_blank' rel="noopener noreferrer">{repo.name}</a>
+        </h1>
+    </div>
+  )
+}
+
+RepoItem.propTypes = {
+    repo: PropTypes.object.isRequired,
+}
+
+export default RepoItem
